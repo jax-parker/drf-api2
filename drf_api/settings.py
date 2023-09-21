da5ada4b4c291de0f2afb9cb32e8ec809196b26d
jax-parker <parkyjax711@gmail.com>
2023-09-21 11:08:22 +0000

    add heroku to allowed user

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -73,7 +73,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),'localhost',]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),'https://taskmaster-drf-api-68d405e0bc1f.herokuapp.com','localhost',]
 
 
 # Application definition
